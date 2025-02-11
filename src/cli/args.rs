c80d9dda25759a1fc8701b7932a800b87c4d043f
Reiner Huechting <dev@reinerhuechting.de>
2025-02-11 21:49:19 +0100

    feat: Make language selectable via cli.

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use std::path::PathBuf;
 
-use crate::exam_tester::exam::ExamInfo;
+use crate::exam_tester::exam::{ExamInfo, ExamInfoLanguage};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -12,6 +12,9 @@ pub struct Args {
     /// The timeout for running the tests in seconds.
     #[arg(short, long, default_value = ExamInfo::test_timeout_default().as_secs().to_string())]
     timeout: u64,
+    /// The language to use for the exam.
+    #[arg(short, long, default_value = "en")]
+    language: ExamInfoLanguage,
 }
 
 impl Args {
