164c26fb01e540c40b85bee73ad7caf8f4771ace
Reiner Huechting <dev@reinerhuechting.de>
2025-06-27 09:11:20 +0200

    test: Add missing test for cli usage of de test exam.

--- tests/cli_usage.rs
+++ tests/cli_usage.rs
@@ -1,5 +1,5 @@
 pub mod common;
-use common::TestCases::GoExamEn;
+use common::TestCases::{GoExamDe, GoExamEn};
 
 use clap::Parser;
 use src2exam::cli::Args;
@@ -12,6 +12,12 @@ fn go_exam_en_dirs() {
         "src2exam",
         "--base-directory",
         &lang.dir_str(),
+        "--tasks-dirname",
+        "tasks",
+        "--submissions-dirname",
+        "submissions",
+        "--grading-dirname",
+        "grading",
         "--timeout",
         "15",
         "--verbose",
@@ -35,17 +41,37 @@ fn go_exam_en_dirs() {
 }
 
 #[test]
-fn go_exam_en_dirs_guessed_language() {
-    let lang = GoExamEn;
+fn go_exam_de_dirs() {
+    let lang = GoExamDe;
 
-    let args = Args::parse_from(["src2exam", "--base-directory", &lang.dir_str()]);
+    let args = Args::parse_from([
+        "src2exam",
+        "--base-directory",
+        &lang.dir_str(),
+        "--tasks-dirname",
+        "aufgaben",
+        "--submissions-dirname",
+        "abgaben",
+        "--grading-dirname",
+        "bewertung",
+        "--timeout",
+        "15",
+        "--verbose",
+        "--dry-run",
+    ]);
 
     let exam_tester = args.exam_tester();
     let exam_config = exam_tester.exam_config();
 
     let exam_dir = lang.dir().canonicalize().unwrap();
     assert_eq!(exam_config.base_dir(), exam_dir);
-    assert_eq!(exam_config.tasks_dir(), exam_dir.join("tasks"));
-    assert_eq!(exam_config.grading_dir(), exam_dir.join("grading"));
-    assert_eq!(exam_config.submissions_dir(), exam_dir.join("submissions"));
+    assert_eq!(exam_config.tasks_dir(), exam_dir.join("aufgaben"));
+    assert_eq!(exam_config.grading_dir(), exam_dir.join("bewertung"));
+    assert_eq!(exam_config.submissions_dir(), exam_dir.join("abgaben"));
+    assert_eq!(
+        exam_config.test_timeout(),
+        std::time::Duration::from_secs(15)
+    );
+    assert_eq!(exam_tester.verbose(), true);
+    assert_eq!(exam_tester.dry_run(), true);
 }
